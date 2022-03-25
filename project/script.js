f82892677080ccbac09de45fec490c85f13a8d5f
anasteisha27 <102163690+anasteisha27@users.noreply.github.com>
2022-03-25 18:10:37 +0300

    Add files via upload

--- project/script.js
+++ project/script.js
@@ -3,17 +3,39 @@
 //alert(a);
 
 
+
 function guess() {
     var a = prompt("Введите число");
     if (a > 5) {
-        alert("chislo menshe ");
+        alert("Число должно быть меньше");
         return guess();
     } else if (a == 5) {
-        return alert("molodets");
+        return alert("Угадали");
     } else {
-        alert("chislo bolshe ");
+        alert("Число должно быть больше");
         return guess();
     }
 }
 
-guess();
+//guess();
+
+function timter() {
+    alert("Ты здесь слишком долго");
+}
+//setTimeout(timter,3000);
+
+function valid() {
+    var name = document.getElementById("name").value;
+    var password = document.getElementById("password").value;
+
+    var reg_name = /^[а-яa-zё]+$/i;
+    var reg_password = /^[a-z0-9]{5,100}]/i;
+    if (reg_name.test(name) == false) {
+        alert("Ошибка в поле ФИО");
+    }
+    if (reg_password.test(password) == false) {
+        alert("Ошибка в поле пароля");
+    }
+}
+
+document.querySelector(".button").addEventListener("click", valid);
